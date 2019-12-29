09c171ea4c389652783f163ee24a0a1419a65b1b
catherine244 <ccayreen24@gmail.com>
2019-12-29 22:05:48 +0300

    added a requests file in the app folder

--- app/views.py
+++ app/views.py
@@ -8,4 +8,14 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    message = 'News Highlight'
+    return render_template('index.html',message = message)
+
+@app.route('/newspage/<int:newspage_id>')
+def newspage(newspage_id):
+
+    '''
+    View news page function that returns the newspage details page and its data
+    '''
+    return render_template('articles.html',id = newspage_id)
