90314adc3d118702bcca1e6ef442ffcc12397ef2
catherine244 <ccayreen24@gmail.com>
2019-12-30 16:23:20 +0300

    modified my files and created the new application structure

--- app/requests.py
+++ app/requests.py
@@ -1,82 +1,92 @@
-from app import app
-import urllib.request,json
-from .models import Source,Articles
-# Getting api key
-api_key = app.config['NEWS_API_KEY']
+import requests
+from .models import Article
+
+# config api and urls
+api_key = None
+top_news_url = None
+top_news_by_source_url = None
+category_news_url = None
+search_news_url = None
+news_sources_url = None
+
+def configure_requests(app):
+    global api_key, top_news_url, top_news_by_source_url, category_news_url, search_news_url, news_sources_url
+    api_key = app.config["NEWS_API_KEY"]
+    top_news_url = app.config["TOP_NEWS_URL"]
+    top_news_by_source_url = app.config["TOP_NEWS_BY_SOURCE_URL"]
+    category_news_url = app.config["CATEGORY_NEWS_URL"]
+    search_news_url = app.config["SEARCH_NEWS_URL"]
+    news_sources_url = app.config["NEWS_SOURCES_URL"]
 
 
-def get_sources(category):
-    '''
-    Function that gets the json response to our url request
-    '''
-    get_sources_url = base_url.format(category,api_key)
 
-    with urllib.request.urlopen(get_sources_url) as url:
-        get_sources_data = url.read()
-        get_sources_response = json.loads(get_sources_data)
+def get_top_news():
+    get_top_news_url = top_news_url.format(api_key)
+    get_news_response = requests.get(get_top_news_url).json()
+    
+    if get_news_response["articles"]:
+        news_results_list = get_news_response["articles"]
+        top_news_results = process(news_results_list)
 
-        source_results = None
+    return top_news_results
 
-        if get_sources_response['sources']:
-            sources_results_list = get_sources_response['sources']
-            sources_results = process_results(sources_results_list)
-    return sources_results
+def get_top_news_by_source(source):
+    get_top_news_by_source_url = top_news_by_source_url.format(source, api_key)
+    get_top_news_by_source_response = requests.get(get_top_news_by_source_url).json()
 
-def process_results(newsource_list):
-    '''
-    Function  that processes the new source result and transform them to a list of Objects
+    if get_top_news_by_source_response["articles"]:
+        news_source_results_list = get_top_news_by_source_response["articles"]
+        top_news_by_source_results = process(news_source_results_list)
 
-    Args:
-        newsource_list: A list of dictionaries that contain news source details
+    return top_news_by_source_results
 
-    Returns :
-        newsource_results: A list of newsource objects
-    '''
-    newsource_results = []
-    for news_item in newsource_list:
-        id = news_item.get('id')
-        name = news_item.get('name')
-        description = news_item.get('description')
-        url = news_item.get('url')
-        category = news_item.get('category')
-        country = news_item.get('country')
+def get_news_by_category(category):
+    get_news_by_category_url = category_news_url.format(category, api_key)
+    category_response = requests.get(get_news_by_category_url).json()
 
-        newsource_object = Source(id,name,description,url,category,country)
-        newsource_results.append(newsource_object)
+    if category_response["articles"]:
+        category_list = category_response["articles"]
+        category_results = process(category_list)
 
-    return newsource_results
+    return category_results
 
-def get_source(source_id,limit):
-    '''
-    Function that gets the json response to our url request
-    '''
-    get_articles_url = article_url.format(source_id,limit,api_key)
-    print(get_articles_url)
+def get_sources():
+    get_news_sources_url = news_sources_url.format(api_key)
+    get_sources_response = requests.get(get_news_sources_url).json()
 
-    with urllib.request.urlopen(get_articles_url) as url:
-        get_articles_data = url.read()
-        get_articles_response = json.loads(get_articles_data)
-        
- 
-        articles_results = None
+    return get_sources_response["sources"]
 
-        if get_articles_response['articles']:
-            articles_results_list = get_articles_response['articles']
-            articles_results = process_articles(articles_results_list)
+def search_news(query):
+    get_search_url = search_news_url.format(query, api_key)
+    search_response = requests.get(get_search_url).json()
 
-    return articles_results
+    if search_response["articles"]:
+        results_list = search_response["articles"]
+        search_results = process(results_list)
+
+    return search_results
+
+def process(news_list):
+    top_news_results = []
+    for item in news_list:
+        author = item.get("author")
+        title = item.get("title")
+        description = item.get("description")
+        url = item.get("url")
+        img = item.get("urlToImage")
+        date = item.get("publishedAt")
+        content = item.get("content")
+
+        if img:
+            news_object = Article(author, title, description, url, img, date, content)
+            top_news_results.append(news_object)
+
+    return top_news_results
 
 
-def process_articles(source):
-    source_articles = []
-    for item_article in source:
-        author = item_article.get('author')
-        title = item_article.get('title')
-        description = item_article.get('description')
-        url = item_article.get('url')
-        urlToImage = item_article.get('urlToImage')
-        publishedAt = item_article.get('publishedAt')
-        if urlToImage:
-            articles_object = Articles(author,title,description,url,urlToImage,publishedAt)
-            source_articles.append(articles_object)
-    return source_articles
+
+
+
+
+
+
--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/cate/Documents/NewsHighlight/virtual"
+set -gx VIRTUAL_ENV "/home/cate/Documents/news/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
