c871e62a7ddcb166f8be3dd1a80e35c79c25b2d3
catherine244 <ccayreen24@gmail.com>
2020-01-09 20:19:19 +0100

    Mofied views.py

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,32 @@ from flask import render_template
 from . import main
 from ..requests import get_top_news ,get_top_news_by_source, get_sources, get_news_by_category, search_news
 
+rom flask import render_template, request, redirect, url_for
+from . import main
+from ..requests import get_topnews, get_categories, get_newsupdates
+
+ #Views
+@main.route('/') 
+def index():
+     '''
+    View root page function that returns the index page and its data
+    '''
+     #Getting top news and categorically arranged news
+     top_articles = get_topnews('general-news')
+     print(top_articles)
+     biz_articles = get_categories('business')
+     tech_articles = get_categories('technology')
+     ent_articles = get_categories('entertainment')
+     sprt_articles = get_categories('sports')
+     title = 'NEWSHIGHLIGHT'
+     return render_template('index.html', title = title, google_news = top_articles, biz = biz_articles, tech = tech_articles, ent = ent_articles, sprt = sprt_articles)
+
+
+@main.route('/update/<id>')
+def article(id):
+    detz_articles = get_newsupdates(id)
+    print(detz_articles)
+    return render_template('news.html',detz = detz_articles)
 @main.route("/")
 def index():
     top_news = get_top_news()
@@ -21,39 +47,6 @@ def index():
                             news = news,
                             categories = categories)
 
-@main.route("/source/<source>")
-def source(source):
-    top_news_by_source = get_top_news_by_source(source)
-    news_source = get_sources()
-    categories = ["business",
-                "entertainment",
-                "general",
-                "health",
-                "science",
-                "sports",
-                "technology"
-                ]
-    return render_template("source.html",
-                            sources = top_news_by_source,
-                            news_source = news_source,
-                            categories = categories,
-                            source = source)
-
-@main.route("/category/<category>")
-def category(category):
-    news_category = get_news_by_category(category)
-    categories = ["business",
-                "entertainment",
-                "general",
-                "health",
-                "science",
-                "sports",
-                "technology"
-                ]
-    return render_template("category.html",
-                            news_category = news_category,
-                            categories = categories,
-                            category = category)
 
 @main.route("/search/<query>")
 def search(query):
