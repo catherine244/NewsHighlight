32753f09c29a33d371ae2a93f4f17503dcd03ead
catherine244 <ccayreen24@gmail.com>
2020-12-30 20:19:19 +0100

    added models.py

--- app/models.py
+++ app/models.py
@@ -1,20 +1,19 @@
-class Article:
-    """
-    Creating class for the news article
-    """
-    def __init__(self, author, title, description, url, img, date, content):
-        self.author = author
-        self.title = title
-        self.description = description
-        self.url = url
-        self.img = img
-        self.date = date
-        self.content = content
-
 class Source:
-    """
-    Creating class for the news source
-    """
-    def __init__(self, id, name):
+    
+    def __init__(self,id,name,description,url,category,country):
         self.id = id
         self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.country = country
+
+class Articles:
+    
+    def __init__(self,author,title,description,url,urlToImage,publishedAt):
+        self.author = author
+        self.title =title
+        self.description = description
+        self.url = url
+        self.urlToImage =urlToImage
+        self.publishedAt = publishedAt
